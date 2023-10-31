d6388f4a812bed1d46b61b8ea42db4de94936106
Your Cheese <sixingzhao@hotmail.com>
2023-10-31 17:23:06 -0500

    get menu items request

--- backend/backend/src/main/java/com/project3/backend/DatabaseController.java
+++ backend/backend/src/main/java/com/project3/backend/DatabaseController.java
@@ -1,13 +1,20 @@
 package com.project3.backend;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RestController
 public class DatabaseController {
-    @GetMapping("/database")
-    public String getDatabase() {
-        System.out.println("Hello World!");
-        return "Hello World!";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/menuItems")
+    public List<Item> getMenuItems() {
+        return jdbcTemplate.query("SELECT * FROM items", new BeanPropertyRowMapper<Item>(Item.class));
     }
 }
---
+++ backend/backend/src/main/java/com/project3/backend/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.project3.backend;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+    List<Item> findByName(String name);
+}
+
+    
