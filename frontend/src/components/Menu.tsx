2ad8bbde633c1d5fadb63a4a2e0b48c46f5e3eda
avanishshah03 <avanishshah03@tamu.edu>
2023-11-01 15:19:20 -0500

    made te menu items into clickable buttons

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import { MenuItem, useMenuStore } from "../store";
 
 export const MenuItemsDisplay = (props: { showImage: boolean }) => {
   const menuItems = useMenuStore((state) => state.menuItems);
   return (
     <div>
-      <Grid container spacing={2}>  
+      <Grid container spacing={2}>
         {menuItems.map((menuItem) => (
           <Grid item key={menuItem.id} xs={12} sm={6} md={4}>
             <Paper
@@ -24,13 +24,18 @@ export const MenuItemsDisplay = (props: { showImage: boolean }) => {
               }}
             >
               {props.showImage && (
-                <Box style={{ height: "100px", width: "100px" }}></Box>
+                <div style={{ height: "100px", width: "100px" }}></div>
               )}
-              <Typography variant="h6">
-                <Box style={{ backgroundColor: "white" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                }}
+              >
+                <Typography variant="h6">
                   {menuItem.name} ${menuItem.price.toFixed(2)}
-                </Box>
-              </Typography>
+                </Typography>
+              </Button>
             </Paper>
           </Grid>
         ))}
