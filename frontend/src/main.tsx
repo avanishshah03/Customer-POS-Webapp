dacc7d232fb35df1ac28c6112ad6b46b90a803d3
avanishshah03 <avanishshah03@tamu.edu>
2023-11-08 09:41:47 -0600

    integrated landing page with app and main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import SignIn from './LoginPage.tsx'
+import LandingPage from './LandingPage.tsx'
+import { Link, Route } from 'wouter'; // Remove Route and import Link
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Route path="/customer"><App /></Route>
+    <Route path="/login"><SignIn /></Route>
+    <Route path="/"><LandingPage /></Route>
   </React.StrictMode>,
 )
