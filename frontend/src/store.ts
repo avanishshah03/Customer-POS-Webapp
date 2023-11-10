8f03c742fac18343dfd2eded8c614d0138092cf6
Deniz Telci <deniza.telci@gmail.com>
2023-11-09 18:51:21 -0600

    Refactor code formatting and add new functionality for adding menu items

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,118 +1,124 @@
 import { create } from "zustand";
 
 interface CartEntry {
-    itemId: number;
-    quantity: number;
+  itemId: number;
+  quantity: number;
 }
 
 export interface MenuItem {
-    id: number;
-    name: string;
-    price: number;
-    imageUrl: string;
-    categoryId: number;
-    vegan: boolean;
-    glutenFree: boolean;
-    size: string;
-    extrasauce: boolean;
+  id: number;
+  name: string;
+  price: number;
+  imageUrl: string;
+  categoryId: number;
+  vegan: boolean;
+  glutenFree: boolean;
+  size: string;
+  extrasauce: boolean;
 }
 
 export interface ItemCategory {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface Order {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
-
-
 
 interface Store {
-
-    checkout: () => void;
-    addCartEntry: (id: number) => void;
-    incrementCartEntryQuantity: (id: number) => void;
-    decrementCartEntryQuantity: (id: number) => void;
-    changeItemPrice: (id: number, newPrice: number) => void;
-    menuItems: MenuItem[];
-    cart: CartEntry[];
-    itemCategories: ItemCategory[];
+  checkout: () => void;
+  addCartEntry: (id: number) => void;
+  incrementCartEntryQuantity: (id: number) => void;
+  decrementCartEntryQuantity: (id: number) => void;
+  changeItemPrice: (id: number, newPrice: number) => void;
+  menuItems: MenuItem[];
+  cart: CartEntry[];
+  itemCategories: ItemCategory[];
 }
 
-let menuItems: MenuItem[] = await (await fetch("/api/menuItems")).json()
-let itemCategories: ItemCategory[] = await (await fetch("/api/itemCategories")).json()
+let menuItems: MenuItem[] = await (await fetch("/api/menuItems")).json();
+let itemCategories: ItemCategory[] = await (
+  await fetch("/api/itemCategories")
+).json();
 export const useMenuStore = create<Store>((set) => ({
-    cart: [],
-    itemCategories: itemCategories,
-    menuItems: menuItems,
-    // TODO: talk to backend
-    setMenuItems: (items: MenuItem[]) => set({ menuItems: items }),
+  cart: [],
+  itemCategories: itemCategories,
+  menuItems: menuItems,
+  // TODO: talk to backend
+  setMenuItems: (items: MenuItem[]) => set({ menuItems: items }),
 
-    addCartEntry: (id: number) => {
-        set(state => {
-            if (state.cart.findIndex(entry => entry.itemId === id) === -1) {
-                return {
-                    cart: [...state.cart, { itemId: id, quantity: 1 }]
-                }
+  addMenuItem: (item: MenuItem) => {
+    fetch("/api/menuItems", {
+      method: "POST",
+      body: JSON.stringify({ ...item, ingredientIds: [], quantities: [] }),
+    });
+    set((state) => ({ menuItems: [...state.menuItems, item] }));
+  },
+
+  addCartEntry: (id: number) => {
+    set((state) => {
+      if (state.cart.findIndex((entry) => entry.itemId === id) === -1) {
+        return {
+          cart: [...state.cart, { itemId: id, quantity: 1 }],
+        };
+      } else {
+        return {
+          cart: state.cart.map((entry) => {
+            if (entry.itemId === id) {
+              return { ...entry, quantity: entry.quantity + 1 };
             } else {
-                return {
-                    cart: state.cart.map(entry => {
-                        if (entry.itemId === id) {
-                            return { ...entry, quantity: entry.quantity + 1 }
-                        } else {
-                            return entry
-                        }
-                    })
-                }
+              return entry;
             }
+          }),
+        };
+      }
+    });
+  },
+  incrementCartEntryQuantity: (id: number) => {
+    set((state) => ({
+      cart: state.cart
+        .map((entry) => {
+          if (entry.itemId == id) {
+            return { ...entry, quantity: entry.quantity + 1 };
+          }
+          return entry;
         })
-    },
-    incrementCartEntryQuantity: (id: number) => {
-        set(state => (
-            {
-                cart: state.cart.map(entry => {
-                    if (entry.itemId == id) {
-                        return { ...entry, quantity: entry.quantity + 1 }
-                    }
-                    return entry
-                }).filter(entry => entry.quantity > 0)
-            }
-        ))
-    },
-    decrementCartEntryQuantity: (id: number) => {
-        set(state => (
-            {
-                cart: state.cart.map(entry => {
-                    if (entry.itemId == id) {
-                        return { ...entry, quantity: entry.quantity - 1 }
-                    }
-                    return entry
-                }).filter(entry => entry.quantity > 0)
-            }
-        ))
-    },
-    changeItemPrice: (id: number, newPrice: number) => {
-        return set(state => ({
-            menuItems: state.menuItems.map(item => {
-                if (item.id === id) {
-                    return { ...item, price: newPrice };
-                }
-                return item;
-            }),
-            cart: state.cart,
-            itemCategories: state.itemCategories
-        }));
-    },
-
-    checkout: () => {
-        set(() => {
-            // TODO: talk to backend
-            return {
-                cart: []
-            }
+        .filter((entry) => entry.quantity > 0),
+    }));
+  },
+  decrementCartEntryQuantity: (id: number) => {
+    set((state) => ({
+      cart: state.cart
+        .map((entry) => {
+          if (entry.itemId == id) {
+            return { ...entry, quantity: entry.quantity - 1 };
+          }
+          return entry;
         })
-    },
+        .filter((entry) => entry.quantity > 0),
+    }));
+  },
+  changeItemPrice: (id: number, newPrice: number) => {
+    return set((state) => ({
+      menuItems: state.menuItems.map((item) => {
+        if (item.id === id) {
+          return { ...item, price: newPrice };
+        }
+        return item;
+      }),
+      cart: state.cart,
+      itemCategories: state.itemCategories,
+    }));
+  },
 
-}))
+  checkout: () => {
+    set(() => {
+      // TODO: talk to backend
+      return {
+        cart: [],
+      };
+    });
+  },
+}));
