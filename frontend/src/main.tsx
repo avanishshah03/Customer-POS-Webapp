4609edd05213025ca7391ba4d8e9ce966aceac10
Deniz Telci <deniza.telci@gmail.com>
2023-11-09 18:07:35 -0600

    Rename app to customer and create server page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './Customer.tsx'
 import SignIn from './LoginPage.tsx'
 import LandingPage from './LandingPage.tsx'
 import { ManagerItems } from "./components/ManagerItems";
 import { Link, Route } from 'wouter'; // Remove Route and import Link
+import { Server } from './components/Server.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Route path="/customer"><App /></Route>
     <Route path="/login"><SignIn /></Route>
     <Route path="/manager"><ManagerItems /></Route>
+    <Route path="/server"><Server /></Route>
     <Route path="/"><LandingPage /></Route>
   </React.StrictMode>,
 )
