32025375489df9333842d71d4fa302f08ac20534
Your Cheese <sixingzhao@hotmail.com>
2023-11-14 19:56:27 -0600

    debugging authorities converter

--- backend/src/main/java/com/project3/backend/config/MappingJwtGrantedAuthoritiesConverter.java
+++ backend/src/main/java/com/project3/backend/config/MappingJwtGrantedAuthoritiesConverter.java
@@ -13,25 +13,40 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.stream.Collectors;
 
 public class MappingJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
     private String authoritiesClaimName = "email";
     private String authorityPrefix = "ROLE_";
-    @Autowired
-    UserServiceImpl userServiceImpl;
+    private Logger logger = Logger.getLogger(MappingJwtGrantedAuthoritiesConverter.class.getName());
+    private FileHandler fh;
+
+    private final UserServiceImpl userServiceImpl = new UserServiceImpl();
 
     @Override
-    @Nullable
     public Collection<GrantedAuthority> convert(Jwt source) {
-        
+        try 
+        {
+            fh = new FileHandler("MappingJwtGrantedAuthoritiesConverter.log");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        logger.info(source.getClaims().toString());
         Collection<String> tokenScopes = parseScopesClaim(source);
         if (tokenScopes.isEmpty()) {
             return Collections.emptyList();
         }
         
         return tokenScopes.stream()
-          .map(s -> this.userServiceImpl.fetchRole(s))
+          .map(this.userServiceImpl::fetchRole)
           .map(s -> this.authorityPrefix + s)
           .map(SimpleGrantedAuthority::new)
           .collect(Collectors.toCollection(HashSet::new));
