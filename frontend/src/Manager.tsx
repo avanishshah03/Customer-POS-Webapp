8a82770a963a37a697d8c668c147607a37f02957
avanishshah03 <avanishshah03@tamu.edu>
2023-12-01 09:35:35 -0600

    Add Sales Report component to Manager

--- frontend/src/Manager.tsx
+++ frontend/src/Manager.tsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import { Tab, Tabs } from '@mui/material';
 import { ManagerItems } from './components/ManagerItems';
 import { ManagerIngredients } from './components/ManagerIngredients';
+import { SalesReport } from './components/SalesReport';
 
 export const Manager: React.FC = () => {
     const [value, setValue] = React.useState(0);
@@ -12,11 +13,12 @@ export const Manager: React.FC = () => {
                 <Tabs value={value} onChange={(_, newValue) => setValue(newValue)}>
                     <Tab label="Items" />
                     <Tab label="Inventory" />
-                    <Tab label="Reports" />
+                    <Tab label="Slaes Report" />
                 </Tabs>
             </Box>
             <div hidden={value != 0}><ManagerItems /></div>
             <div hidden={value != 1}><ManagerIngredients /></div>
+            <div hidden={value != 2}><SalesReport /></div>
         </>
     )
 
