81b4315a07a92f27817d35185513fc6c4650f0d1
enagelc <enagelc@gmail.com>
2023-12-04 16:59:26 -0600

    reqs in orders page

--- frontend/src/components/OrdersPage.tsx
+++ frontend/src/components/OrdersPage.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import OrderTable from './OrderTable';
 
 export const OrdersPage: React.FC = () => {
     return (
         <div>
             <h1>Orders Page</h1>
+            <ul>
+                <li>Create: make a new order</li>
+                <li>Read: view all orders; view a single order (including status: pending, fulfilled, cancelled, etc.)</li>
+                <li>Update: modify an order (add/remove items, change status, etc.)Order 2</li>
+                <li>Delete: remove an order from the order history</li>
+            </ul>
+            <OrderTable />
         </div>
     );
 };
