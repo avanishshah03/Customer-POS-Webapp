1ee28001a5b96955662a4753ad584868815d73d6
ighedosadt-tamu <142918078+ighedosadt-tamu@users.noreply.github.com>
2023-12-05 17:14:00 -0600

    trying to pull

--- frontend/src/LandingPage.tsx
+++ frontend/src/LandingPage.tsx
@@ -21,25 +21,6 @@ export const LandingPage: React.FC = () => {
         color: 'white',
     };
     const { role, signOut } = useContext(AuthContext);
-    const googleTranslateElementInit = () => {
-        new window.google.translate.TranslateElement(
-          {
-            pageLanguage: "en",
-            autoDisplay: false
-          },
-          "google_translate_element"
-        );
-      };
-      useEffect(() => {
-        var addScript = document.createElement("script");
-        addScript.setAttribute(
-          "src",
-          "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        );
-        document.body.appendChild(addScript);
-        window.googleTranslateElementInit = googleTranslateElementInit;
-      }, []);
-
     return (
         <div className="landing-page background-image" >
             <div className="content" id="google_translate_element">
