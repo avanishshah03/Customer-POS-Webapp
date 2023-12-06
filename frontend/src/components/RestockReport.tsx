5a05feecedf4ac949bb03b50d9f8e09c59caa320
avanishshah03 <avanishshah03@tamu.edu>
2023-12-05 23:29:28 -0600

    created restock report graph using chart.js

--- frontend/src/components/RestockReport.tsx
+++ frontend/src/components/RestockReport.tsx
@@ -1,73 +1,77 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "../config/axiosConfig";
-import {
-  Paper,
-  Typography,
-  TableContainer,
-  Table,
-  TableHead,
-  TableRow,
-  TableCell,
-  TableBody,
-  TextField,
-  Button,
-} from "@mui/material";
+import { Paper, Typography } from "@mui/material";
+import Chart from "chart.js/auto";
 
-export const RestockReport = () => {
-  const [restockData, setRestockData] = useState([]);
+interface RestockData {
+    ingredient: {
+        name: string;
+        stock: number;
+        restock: number;
+    };
+}
 
-  useEffect(() => {
-    axios.get("/restockReport").then((res) => {
-      setRestockData(res.data);
-      console.log(res.data);
-    });
-  }, []);
+export const RestockReport: React.FC = () => {
+    const [restockData, setRestockData] = useState<RestockData[]>([]);
+    const chartRef = useRef<HTMLCanvasElement | null>(null);
 
-  const tableStyle = { width: "100%" };
-  const cellStyle = { padding: "8px" };
+    useEffect(() => {
+        axios.get<RestockData[]>("/restockReport").then((res) => {
+            setRestockData(res.data);
+            console.log(res.data);
+        });
+    }, []);
 
-  return (
-    <Paper
-      style={{
-        padding: "20px",
-        margin: "10px",
-        backgroundColor: "#f3f3f3",
-        fontSize: "2.9em",
-      }}
-    >
-      <Typography variant="h5" style={{ textAlign: "center" }}>
-        Restock Report
-      </Typography>
-      <TableContainer>
-        <Table style={tableStyle}>
-          <TableHead>
-            <TableRow>
-              <TableCell style={cellStyle}>
-                <Typography variant="h6">Ingredient</Typography>
-              </TableCell>
-              <TableCell style={cellStyle}>
-                <Typography variant="h6">Stock ammount</Typography>
-              </TableCell>
-              <TableCell style={cellStyle}>
-                <Typography variant="h6">Restock ammount</Typography>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {restockData.map((rowData) => (
-              <TableRow key={rowData.restockAmmount}>
-                <TableCell style={cellStyle}>{rowData.ingredient.name}</TableCell>
-                <TableCell style={cellStyle}>
-                  {rowData.ingredient.stock}
-                </TableCell>
-                <TableCell style={cellStyle}>
-                  {rowData.ingredient.restock}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
-  );
+    useEffect(() => {
+        if (restockData.length > 0 && chartRef.current) {
+            const ctx = chartRef.current.getContext("2d");
+
+            if (ctx) {
+                const labels = restockData.map((rowData) => rowData.ingredient.name);
+                const stockData = restockData.map((rowData) => rowData.ingredient.stock);
+                const restockDataPoints = restockData.map((rowData) => rowData.ingredient.restock);
+
+                new Chart(ctx, {
+                    type: "bar",
+                    data: {
+                        labels,
+                        datasets: [
+                            {
+                                label: "Stock Amount",
+                                data: stockData,
+                                backgroundColor: "rgba(75,192,192,0.2)",
+                                borderColor: "rgba(75,192,192,1)",
+                                borderWidth: 1,
+                            },
+                            {
+                                label: "Restock Amount",
+                                data: restockDataPoints,
+                                backgroundColor: "rgba(255,99,132,0.2)",
+                                borderColor: "rgba(255,99,132,1)",
+                                borderWidth: 1,
+                            },
+                        ],
+                    },
+                    options: {
+                        scales: {
+                            x: { stacked: true },
+                            y: { stacked: true },
+                        },
+                    },
+                });
+            }
+        }
+    }, [restockData]);
+
+    return (
+        <Paper style={{ padding: "20px", margin: "10px", backgroundColor: "#f3f3f3", fontSize: "2.9em" }}>
+            <Typography variant="h5" style={{ textAlign: "center" }}>
+                Restock Report
+            </Typography>
+            {restockData.length > 0 && <canvas ref={chartRef}></canvas>}
+        </Paper>
+    );
 };
+
+
+
